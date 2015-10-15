b8f8fb97a9e90646848d86253cb666170e91314a
Girish <girish@post.com>
2015-10-15 14:53:47 -0700

    added hash

---
+++ src/ElmahR/Controllers/HashController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using ElmahR.Models;
+
+namespace ElmahR.Controllers
+{
+    public class HashController : Controller
+    {
+        [AllowAnonymous]
+        public string Index(string id = "")
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                var hash = HashGenerator.GetHashString(id);
+                ViewBag.Message = hash;
+                return hash;
+            }
+            return string.Empty;
+        }
+    }
+}
